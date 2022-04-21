c63e8f463fb380d651bd7486221cf7892f67140e
IvanRG00 <ivan402000@abv.bg>
2022-04-21 16:32:17 +0300

    Commit

---
+++ Person.java
@@ -0,0 +1,3 @@
+Êþº¾   4   bg/unwe/Person  java/lang/Object getName ()Ljava/lang/String; Setname (Ljava/lang/String;)V MethodParameters name 
+SourceFile Person.java               	    
+        
