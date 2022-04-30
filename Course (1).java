19591d17f36cce483451de7826cf3716b8263c41
IvanRG00 <103127148+IvanRG00@users.noreply.github.com>
2022-04-30 15:16:56 +0300

    Update Course (1).java

--- Course (1).java
+++ Course (1).java
@@ -13,18 +13,18 @@ import java.util.ArrayList;
 public class Course {
 
     private String name;
-
+    private ArrayList<Student> students;
+    
+    public Course() {
+        students = new ArrayList<>();
+    }
+   
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
-    }
-    ArrayList<Student> students;
-
-    public Course() {
-        students = new ArrayList<>();
     }
 
     public void addStudent(Student student) {
